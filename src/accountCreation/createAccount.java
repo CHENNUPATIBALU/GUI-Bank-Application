dc11140c09784d419a9555d11173e35b1f3475a3
Chennupati Balu <57926813+CHENNUPATIBALU@users.noreply.github.com>
2020-09-18 13:06:52 +0530

    updated project

--- src/accountCreation/createAccount.java
+++ src/accountCreation/createAccount.java
@@ -1,29 +1,30 @@
-package customerUI;
+package accountCreation;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Random;
 
 import javax.swing.*;
-import bankDAO.*;
+import bankDAO.SavingsDAO;
+import customerUI.CurrentAccount;
+import customerUI.SavingsAccount;
+import bankDAO.CurrentDAO;
 
-public class createAccount extends WindowAdapter implements ActionListener,ItemListener{
+public class createAccount extends WindowAdapter implements ActionListener{
 
 	JFrame f;
 	JTextField t1,t2;
 	JButton b1;
 	JCheckBox cb1,cb2;
 	JLabel l1,l2,l3;
-	SavingsDAO sd;
-	CurrentDAO cd;
 	int accno;
 	
 	static Random rand = new Random();
 	
-	public createAccount() throws Exception
+	
+	public createAccount()
 	{
 		f = new JFrame("Account Creation");
 		t1 = new JTextField();
@@ -34,23 +35,22 @@ public class createAccount extends WindowAdapter implements ActionListener,ItemL
 		
 		b1 = new JButton("Create");
 		
-		sd = new SavingsDAO();
-		cd = new CurrentDAO();
 		
 		l1 = new JLabel("Enter your Name: ");
 		l2 = new JLabel("Your Account Number: ");
 		l3 = new JLabel("Enter the amount for initial deposit: ");
 		
-		l1.setBounds(10, 20, 80, 30);
-		l2.setBounds(10, 50, 80, 30);
+		l1.setBounds(10, 20, 150, 30);
+		l2.setBounds(10, 50, 200, 30);
+		l3.setBounds(10, 80, 300, 30);
 		
-		t1.setBounds(200, 20, 50, 30);
-		t2.setBounds(200, 50, 50, 30);
+		t1.setBounds(210, 20, 150, 30);
+		t2.setBounds(210, 80, 150, 30);
 		
-		b1.setBounds(210, 80, 80, 30);
+		b1.setBounds(250, 200, 80, 30);
 		
-		cb1.setBounds(210, 70, 100, 30);
-		cb2.setBounds(270, 70, 100, 30);
+		cb1.setBounds(210, 120, 100, 30);
+		cb2.setBounds(240, 150, 100, 30);
 		
 		
 		f.add(l1);
@@ -65,10 +65,10 @@ public class createAccount extends WindowAdapter implements ActionListener,ItemL
 		t1.addActionListener(this);
 		t2.addActionListener(this);
 		b1.addActionListener(this);
-		cb1.addItemListener(this);
-		cb2.addItemListener(this);
+		cb1.addActionListener(this);
+		cb2.addActionListener(this);
 		
-		f.setSize(400,300);
+		f.setSize(700,300);
 		f.setVisible(true);
 		f.setLayout(null);
 		f.addWindowListener(this);
@@ -76,13 +76,29 @@ public class createAccount extends WindowAdapter implements ActionListener,ItemL
 	
 	public void createSavingsAccount(int accno,String name,float amount) throws Exception
 	{
+		SavingsDAO sd;
+		SavingsAccount sa;
+		sd = new SavingsDAO();
+		sa = new SavingsAccount();
 		accno=rand.nextInt(999999999);
 		sd.insertSavingsTb(accno, name, amount);
+		sa.setSavingsnameDetails(name, accno, amount);
+		
 	}
 	public void createCurrentAccount(int accno,String name,float amount) throws Exception
 	{
+		CurrentDAO cd;
+		CurrentAccount ca;
+		cd = new CurrentDAO();
+		ca = new CurrentAccount();
 		accno=rand.nextInt(999999999);
 		cd.insertCurrentTb(accno, t1.getText(), Float.parseFloat(t2.getText()));
+		ca.setCurrentnameDetails(name, accno, amount);
+	}
+	
+	public void windowClosing(WindowEvent e)
+	{
+		System.exit(0);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -94,7 +110,7 @@ public class createAccount extends WindowAdapter implements ActionListener,ItemL
 			}
 			catch(Exception ae) {}
 		}
-		else if(e.getSource()==cb2&& e.getSource()==b1)
+		else if(e.getSource()==cb2 && e.getSource()==b1)
 		{
 			try
 			{
@@ -102,29 +118,5 @@ public class createAccount extends WindowAdapter implements ActionListener,ItemL
 			}
 			catch(Exception ae) {}
 		}
-	}
-	@Override
-	public void itemStateChanged(ItemEvent e) {
-		/*if(e.getSource()==cb1 && e.getStateChange()==1 && e.getSource()==b1)
-		{
-			try
-			{
-				accno=rand.nextInt(999999999);
-				sd.insertSavingsTb(accno, t1.getText(), Float.parseFloat(t2.getText()));
-			}
-			catch(Exception ae) {}
-		}
-		else if(e.getSource()==cb2 && e.getStateChange()==1 && e.getSource()==b1)
-		{
-			try
-			{
-				accno=rand.nextInt(999999999);
-				cd.insertCurrentTb(accno, t1.getText(), Float.parseFloat(t2.getText()));
-			}
-			catch(Exception ae) {}
-		}*/
-		
-		
-		
 	}
 }
